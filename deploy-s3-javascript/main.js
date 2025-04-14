8e9a2c7ca120a53fd926b33a56036fc54f89f0ba
Nikita Kashyap <161322388+nikitaks97@users.noreply.github.com>
2025-04-14 16:26:07 +0200

    Update main.js

--- deploy-s3-javascript/main.js
+++ deploy-s3-javascript/main.js
@@ -3,7 +3,13 @@ const github= require('@actions/github');
 const exec= require('@actions/exec');
 
 function run() {
+    // get input values
+ const bucket =  core.getInput('bucket' , { required:true });       //retrive the imputs from action.yml file and store it in a constant
+ const bucketRegion =  core.getInput('bucketRegion' , { required:true });
+ const distFolder = core.getInput('dist-folder', { required:true });
     core.notice('Hello from custom js action!')
-
+  // upload files
+ const s3Uri = `s3://${bucket}`;
+ exec.exec('aws s3 sync ${distFolder}  ${s3Uri} ${bucketRegion}`);
 }
  run();
